f12882f13d0b9660e2d58fdc25aa9972fb81a27d
John van Dijk <john@fortyfour-three.com>
2023-03-18 07:58:13 -0400

    Added the reduce counter increments.

--- game/food.py
+++ game/food.py
@@ -1,5 +1,12 @@
 class Food:
-    remaining_countdown = 0
+    remaining_countdown: int = 0
     colour = 0
-    def __init__(self):
-        pass
+
+    def reduce_at_normal_speed(self):
+        self.remaining_countdown += -1
+
+    def reduce_at_increased_speed(self):
+        self.remaining_countdown += -2
+
+    def __init__(self, countdown_start: int):
+        self.remaining_countdown = countdown_start
--- tests/test_food.py
+++ tests/test_food.py
@@ -1,10 +1,20 @@
 import unittest
-
+import game.food as food
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+    def test_countdown_start(self):
+        f = food.Food(10)
+        self.assertEqual(f.remaining_countdown, 10)
 
+    def test_reduce_at_normal_speed(self):
+        f = food.Food(10)
+        f.reduce_at_normal_speed()
+        self.assertEqual(f.remaining_countdown, 9)
+
+    def test_reduce_at_increased_speed(self):
+        f = food.Food(10)
+        f.reduce_at_increased_speed()
+        self.assertEqual(f.remaining_countdown, 8)
 #
 # if __name__ == '__main__':
 #     unittest.main()
