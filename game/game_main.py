b9e7719f50002de7aed57f5d4166208bcd7250a6
John van Dijk <john@fortyfour-three.com>
2023-03-18 08:39:05 -0400

    Set up a class for the main game loop.

---
+++ game/game_main.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Main:
+    running = True
+    counter = 0
+
+    def game_loop(self):
+        while self.running:
+            print("something", self.counter)
+
+            self.counter += 1
+
+            if self.counter == 10:
+                self.running = False
+
+    def __init__(self):
+        pygame.init()
+        self.game_loop()
+
+# https://www.geeksforgeeks.org/how-to-set-up-the-game-loop-in-pyggame/
--- main.py
+++ main.py
@@ -4,14 +4,20 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import variables
+import pygame
+import game.game_main
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {variables.COLOURS.RED.value}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def main():
+    new_game = game.game_main.Main()
+    new_game.game_loop()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
